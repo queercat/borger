d9a4d2d36d1091b87b9d1c46942a61b5b65edf5c
May Tusek <128516535+may-20i@users.noreply.github.com>
2024-02-28 12:47:39 -0800

    Fixes tokenizer behavior.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use crate::tokenizer::tokenize;
 pub mod tokenizer;
 
 fn main() {
-    let tokens = tokenize("hello world");
+    let tokens = tokenize("hello world 123   \n 456");
 
     println!("{:?}", *tokens)
 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,12 +1,50 @@
 #[derive(Debug)]
-pub struct Token<'a> {
-    pub text: &'a str
+pub struct Token {
+    pub text: String
 }
 
 pub fn tokenize(source: &str) -> Box<Vec<Token>> {
     let mut tokens: Box<Vec<Token>> = Box::new(Vec::new());
+    let mut chars = source.chars().peekable();
 
-    source.split_whitespace().for_each(|t| tokens.push(Token {text: t }));
+    loop {
+        if chars.peek() == None { break; }
+
+        let mut char = chars.next().unwrap();
+        let mut text = String::new();
+
+        if match_whitespace(char) {
+            loop {
+                if chars.peek() == None { return tokens; }
+
+                let next = *chars.peek().unwrap();
+                char = chars.next().unwrap();
+
+                if !match_whitespace(next) { break; }
+            }
+        }
+
+        if match_alphanumeric(char) {
+            loop {
+                text.push(char);
+
+                if chars.peek() == None { break; }
+                if !match_alphanumeric(*chars.peek().unwrap()) { break; }
+
+                char = chars.next().unwrap();
+            }
+        }
+
+        tokens.push(Token { text })
+    }
 
     return tokens;
+}
+
+fn match_alphanumeric(char: char) -> bool {
+   (char >= '0' && char <= '9') || (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
+}
+
+fn match_whitespace(char: char) -> bool {
+    char == ' ' || char == '\n' || char == '\t'
 }
