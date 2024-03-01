6c057e1f9f07137cc4207a9c4a2386061f4879df
May Tusek <tusek.may@gmail.com>
2024-02-29 20:06:35 -0800

    Deletes reader, and moves functionality to main.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
-use crate::reader::repl;
+use std::io::{stdin, stdout, Write};
 
-pub mod reader;
+use parser::{read_form, BorgerType};
+use tokenizer::tokenize;
+
+mod parser;
 mod test;
 pub mod tokenizer;
-mod parser;
 
 fn main() {
-    repl();
+    loop {
+        let ast = eval(read());
+
+        println!("{:?}", ast);
+    }
+}
+
+fn eval(source: String) -> BorgerType {
+    let binding = tokenize(source.as_str());
+    let mut tokens = binding.iter().peekable();
+
+    let ast = read_form(&mut tokens);
+
+    ast
+}
+
+pub fn read() -> String {
+    print!("> ");
+    let result = stdout().flush();
+
+    if result.is_err() {
+        panic!("Unable to flush STDOUT.");
+    }
+
+    let mut input = String::new();
+    let result = stdin().read_line(&mut input);
+
+    if result.is_err() {
+        panic!("Unable to read line for REPL.");
+    }
+
+    input
 }
